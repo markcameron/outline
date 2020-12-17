e17883c4f900916850009f818d2e3b48220ab2a3
Mark Cameron <mark.cameron@devfactory.ch>
2020-12-18 00:07:59 +0100

    Fix location of index.js for 'pm2-docker start' command

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd /opt/outline
 
 yarn sequelize:migrate --env production-ssl-disabled
 
-env NODE_ENV=production pm2-docker start index.js --name outline
+env NODE_ENV=production pm2-docker start ./build/server/index.js --name outline
